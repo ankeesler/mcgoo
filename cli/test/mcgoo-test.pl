0d962853e6f0447e58adb8401f0ad6e66df6d722
Andrew Keesler <ankeesler1@gmail.com>
2014-11-22 17:03:37 -0500

    Add a check that the makefile exists.

--- cli/test/mcgoo-test.pl
+++ cli/test/mcgoo-test.pl
@@ -8,14 +8,21 @@ use warnings;
 use lib '../..';
 use Mcgoo;
 
+my $EXEC = "./build/mcgoo";
+
 Mcgoo::Announce;
 
 `make clean`;
 `make compile`;
 
-my $usage = `./build/mcgoo`;
+my $usage = `$EXEC`;
 chomp $usage;
 
 Mcgoo::Expect $usage, qw/usage:.*/;
+
+my $output = `$EXEC`;
+chomp $output;
+
+Mcgoo::FileExists "Makefile";
 
 Mcgoo::Summarize;
