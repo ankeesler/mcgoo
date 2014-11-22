d6230fdc4a95ad288bd6e0a2adaeeae43ff163a3
Andrew Keesler <ankeesler1@gmail.com>
2014-11-22 16:54:05 -0500

    Produce open and close APIs for makefile.

--- cli/makefile.c
+++ cli/makefile.c
@@ -22,8 +22,23 @@ int MakefileCreate(const char *filename)
   if (makefile)
     return MAKEFILE_ALREADY_CREATED;
 
-  if (!(makefile = fopen(filename, "w")))
+  if (!(makefile = fopen(filename, "w+")))
     return MAKEFILE_CANNOT_OPEN;
 
   return MAKEFILE_SUCCESS;
+}
+
+
+int MakefileClose(void)
+{
+  int retVal = MAKEFILE_SUCCESS;
+  
+  if (!makefile)
+    retVal = MAKEFILE_NOT_CREATED;
+  else
+    retVal = (fclose(makefile) ? MAKEFILE_CANNOT_CLOSE : MAKEFILE_SUCCESS);
+  
+  makefile = NULL;
+  
+  return retVal;
 }
--- cli/makefile.h
+++ cli/makefile.h
@@ -13,9 +13,15 @@
 // Errors.
 #define MAKEFILE_SUCCESS 0
 #define MAKEFILE_ALREADY_CREATED 1
-#define MAKEFILE_CANNOT_OPEN 2
+#define MAKEFILE_NOT_CREATED 2
+#define MAKEFILE_CANNOT_OPEN 3
+#define MAKEFILE_CANNOT_CLOSE 4
 
 // Create a makefile will the filename.
+// If the file is already created, it appends to it.
 int MakefileCreate(const char *filename);
+
+// Close the makefile. Don't forget to call this!
+int MakefileClose(void);
 
 #endif /* __MAKEFILE_H__ */
